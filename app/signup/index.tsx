aa577da09f12aecd773efa17181e49c5b80ba28f
birlaPranjal <birlapranjal460@gmail.com>
2025-08-03 20:31:15 +0530

    Add SignUpScreen component for user registration

---
+++ app/signup/index.tsx
@@ -0,0 +1,12 @@
+//react native login page
+import { Text, View } from "react-native";
+
+const SignUpScreen = () => {
+  return (
+    <View>
+      <Text>Sign Up</Text>
+    </View>
+  );
+};
+
+export default SignUpScreen;
