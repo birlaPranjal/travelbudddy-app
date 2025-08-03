91c7e5464ecd3214bb0d7d7963d7b4fb610e13cc
birlaPranjal <birlapranjal460@gmail.com>
2025-08-03 20:30:01 +0530

    Update Index component text and add empty login and signup components

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text> hii how are yiu </Text>
     </View>
   );
 }
